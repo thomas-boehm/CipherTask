a8817b7756de0cc9f5d4db979ee32e0f6c527634
Thomas Boehm <thomasboehm29@gmail.com>
2019-04-04 07:44:58 +0000

    Task 1 and 2 completed

--- cipher.c
+++ cipher.c
@@ -23,7 +23,7 @@ int main()  {
 //Function definitions: 
 //Men
 void menu(void) {
-    static int menuInput;                                                                       //user's selection
+    static int menuInput = 0;                                                                       //user's selection
     do  {
         clrscr();
         printf("Enter the number that corresponds to the program you wish to access: \n\n");
@@ -48,7 +48,7 @@ void menu(void) {
             break;
             case 6: task6();
             break;
-            case 7: return 0;
+            case 7: exit(7);
             default: printf("Invalid entry, please try again. ");
         } 
     }   while(menuInput!=1 && menuInput!=2 && menuInput!=3 && menuInput!=4 && menuInput!=5 && menuInput!=6 && menuInput!=7);    //checks if input is valid (do while loop was not functioning)
@@ -59,24 +59,25 @@ void menu(void) {
 int task1(void) {   
     clrscr();                                                                                   
     int i, key;                                                                                 //"i" is a counter (index), "key" is the amount of rotation
+    int ran;
     char str[100];                                                                              //"str[100]" is the string of text to be encrypted of maximum length, 100
     printf("The encryption of text via a rotation cipher, given the text and the key:\n\n");    //a heading to remind the reader what this task does
-    printf("To begin, enter the text you wish to encrypt.\n");                                  //a prompt to enter a key
+    printf("Enter the text you wish to encrypt, followed by a key between 0 and 26 to determine the amount of rotation\n");  //a prompt to enter text and a key
     printf("   Text: ");
     fgets(str, 100, stdin);
-    scanf("");
-    printf("Now enter a key between 0 and 26 to determine the amount of rotation.\n   Key: ");  //a prompt to enter the text to be encrypted
+    printf("%s", str);
+    printf("   ");
     scanf("%d", &key);                                                                          //assigns the input text to the array "str[]"
-    if(key >= 0 && key <= 26)    {
+    if(key >= 0 && key <= 26)    {                                                              //checks for an appropriate key
         for(i = 0; i < strlen(str); i++)   {                                                    //converts each letter of the input string to the encrypted letter
-            if(str[i] >= 97)    {                                                               //checks for lower-case letters
-                if(str[i] + key > 122 && str[i] < 123)    {                                     //checks if the letter is bigger than 'z'
-                    str[i] -= 26;                                                               //subtracts 26 to turn 'z + 1' into 'a'
+            if(str[i] >= 97 && str[i] <= 122)    {                                              //checks for lower-case letters
+                if(str[i] + key > 122)    {                                                     //checks if the letter is bigger than 'z'
+                    str[i] -= 26;                                                               //subtracts 26 to shift 'z + 1' into 'a'
                 }
                 str[i] += key;                                                                  //adds the value of "key" to the ASCII code of the letter
             } else if(str[i] >= 65 && str[i] <= 90)  {                                          //checks for upper-case letters
                 if(str[i] + key > 90)    {                                                      //checks if the letter is bigger than 'Z'
-                    str[i] -= 26;                                                               //subtracts 26 to turn 'Z + 1' into 'A'
+                    str[i] -= 26;                                                               //subtracts 26 to shift 'Z + 1' into 'A'
                 }
                 str[i] += key;                                                                  //adds the value of "key" to the ASCII code of the letter
             }
@@ -93,53 +94,49 @@ int task1(void) {
             menu();
         }
     } while(a != 1);  
-    return;
+    return 0;
 }
 /*---------------------------------------------------------------------------*/
 // 2. Rotation decryption (given text & key):
 int task2(void) {
-    clrscr();                                                                                   //clears screen
+    clrscr();                                                                                   
     int i, key;                                                                                 //"i" is a counter (index), "key" is the amount of rotation
-    char str[100];                                                                              //"str[100]" is the string of text to be decrypted of maximum length, 100
+    int ran;
+    char str[100];                                                                              //"str[100]" is the string of text to be encrypted of maximum length, 100
     printf("The decryption of text via a rotation cipher, given the text and the key:\n\n");    //a heading to remind the reader what this task does
-    printf("To begin, enter a key between 0 and 26 to determine the amount of rotation.\n");    //a prompt to enter a key
-    printf("   Key: ");
-    scanf("%d", &key);                                                                          //assigns the input key to the variable, "key"
-    if(key >= 0 && key <= 26)    {                                                              //check for an approriate key
-        printf("Now enter the text to be decrypted: \n   Text: ");                              //a prompt to enter the text to be decrypted
-        fgets(str, 100, stdin);                                                                 //assigns the input text to the array "str[]"
-        for(i = 0; i < strlen(str); i++)   {                                                    //converts each letter of the input string to the decrypted letter                                                                  //checks for whitespace
-                if(str[i] >= 97)    {                                                           //checks for lower-case letters
-                    if(str[i] - key < 97)    {                                                  //checks if the letter is smaller than 'a'
-                        str[i] += 26;                                                           //adds 26 to turn 'z + 1' into 'a' if necessary
-                    }
-                    str[i] -= key;                                                              //adds the value of "key" to the ASCII code of the letter
-                } else if(str[i] >= 65 && str[i] <= 90)  {                                      //checks for upper-case letters
-                    if(str[i] - key < 65)    {                                                  //checks if the letter is smaller than 'A'
-                        str[i] += 26;                                                           //adds 26 to turn 'Z + 1' into 'A' if necessary
-                    }
-                    str[i] -= key;                                                              //subtracts the value of "key" from the ASCII code of the letter
+    printf("Enter the text you wish to decrypt, followed by a key between 0 and 26 to determine the amount of rotation\n");  //a prompt to enter text and a key
+    printf("   Text: ");
+    fgets(str, 100, stdin);
+    printf("%s", str);
+    printf("   ");
+    scanf("%d", &key);                                                                          //assigns the input text to the array "str[]"
+    if(key >= 0 && key <= 26)    {                                                              //checks for an appropriate key
+        for(i = 0; i < strlen(str); i++)   {                                                    //converts each letter of the input string to the encrypted letter
+            if(str[i] >= 97 && str[i] <=122)    {                                               //checks for lower-case letters
+                if(str[i] - key < 97)    {                                                      //checks if the letter is bigger than 'z'
+                    str[i] += 26;                                                               //adds 26 to shift 'a - 1' into 'z'
                 }
+                str[i] -= key;                                                                  //subtracts the value of "key" from the ASCII code of the letter
+            } else if(str[i] >= 65 && str[i] <= 90)  {                                          //checks for upper-case letters
+                if(str[i] - key < 65)    {                                                      //checks if the letter is smaller than 'A'
+                    str[i] += 26;                                                               //adds 26 to shift 'A - 1' into 'Z'
+                }
+                str[i] -= key;                                                                  //subtracts the value of "key" from the ASCII code of the letter
+            }
         }
-            printf("\n\nThe decrypted message reads: %s\n", str);                             //prints the decrypted message
+        printf("\nThe decrypted message reads: %s\n\n", str);                                   //prints the decrypted message
     }   else    {
         task2();
-    }
-    int a;                                                                                      //sub-menu for repating the task returning to the menu
-    do {
-        printf("\n\nSelect an option:");    //a divider from the previous text, without using "clrscr()" function
-        printf("   1. Restart task 2\n");
-        printf("   2. Proceed to task 3\n");
-        printf("   3. Return to menu\n\nSelection: ");
-        scanf("%d", &a);
-        switch(a)   {
-            case 1: clrscr();
-                    task2();
-            case 2: menu();
-            default: clrscr(); 
         }
-    } while(a != 1 && a != 2);    
-    return;
+    int a = 0;                                                                                  //option to return to main menu
+    do {
+        printf("Enter 1 to return to main menu: ");
+        scanf("%d", &a);
+        if(a == 1)  {
+            menu();
+        }
+    } while(a != 1);  
+    return 0;
 }
 /*---------------------------------------------------------------------------*/
 // 3. Substitution encryption (given text & key):
